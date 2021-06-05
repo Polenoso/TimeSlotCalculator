e56e9c5bbbf8fe2d6875e3f619f3d1d94a3fa074
Aitor Pagán <aitor.pagan@wallapop.com>
2021-06-05 15:42:18 +0200

    Return flat if hour is earlier than 12

--- Sources/TimeSlotsCalculator/TimeSlotsCalculator.swift
+++ Sources/TimeSlotsCalculator/TimeSlotsCalculator.swift
@@ -1,7 +1,14 @@
 import Foundation
 
 public struct TimeSlotsCalculator {
+    private static let dateComponents: Set<Calendar.Component> = [.day, .month, .year, .hour, .minute, .second]
     public func getTimeSlot(for date: Date) -> TimeSlot {
+        let components = Calendar.current.dateComponents(Self.dateComponents,
+                                           from: date)
+        if let hour = components.hour, hour < 12 {
+            return .flat
+        }
+        
         return .rush
     }
 }
